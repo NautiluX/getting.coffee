1faa010b0f8cb018a194b98f28938e7aa60b9357
Manuel Dewald <mdewald@redhat.com>
2020-12-08 09:21:01 +0100

    make coffee engine configurable

--- coffee.go
+++ coffee.go
@@ -12,23 +12,49 @@ import (
 	"time"
 )
 
+type CoffeeEngine struct {
+	config Config
+}
+
 func main() {
-	fs := http.FileServer(http.Dir("./content"))
-	http.Handle("/favicon.ico", fs)
-	http.Handle("/icon.png", fs)
-	http.Handle("/site.webmanifest", fs)
-	http.HandleFunc("/", index)
-	http.HandleFunc("/img/", serveGif)
-	http.ListenAndServe(":8080", nil)
+	config := Config{os.Getenv("COFFEE_TITLE"), os.Getenv("COFFEE_DESCRIPTION"), os.Getenv("COFFEE_GIFDIR")}
+	config.HealthCheck()
+	c := CoffeeEngine{config}
+	c.StartServer()
 }
 
 type Config struct {
 	Title       string
 	Description string
+	GifDir      string
 }
 
-func index(res http.ResponseWriter, req *http.Request) {
-	config := Config{"Getting coffee, brb ...", "Be right back!"}
+func (c *Config) HealthCheck() {
+	if c.GifDir == "" {
+		panic(fmt.Errorf("environment variable COFFEE_GIFDIR not set. Direct it to a folder with gifs"))
+	}
+	if c.Title == "" {
+		panic(fmt.Errorf("environment variable COFFEE_TITLE not set."))
+	}
+	if c.Description == "" {
+		panic(fmt.Errorf("environment variable COFFEE_DESCRIPTION not set."))
+	}
+	if _, err := os.Stat(c.GifDir); os.IsNotExist(err) {
+		panic(fmt.Errorf("Path %s doesn't exist", c.GifDir))
+	}
+}
+
+func (c *CoffeeEngine) StartServer() {
+	fs := http.FileServer(http.Dir("./content"))
+	http.Handle("/favicon.ico", fs)
+	http.Handle("/icon.png", fs)
+	http.Handle("/site.webmanifest", fs)
+	http.HandleFunc("/", c.index)
+	http.HandleFunc("/img/", c.serveGif)
+	http.ListenAndServe(":8080", nil)
+}
+
+func (c *CoffeeEngine) index(res http.ResponseWriter, req *http.Request) {
 	fp := path.Join("content", "index-template.html")
 	tmpl, err := template.ParseFiles(fp)
 	if err != nil {
@@ -36,12 +62,12 @@ func index(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if err := tmpl.Execute(res, config); err != nil {
+	if err := tmpl.Execute(res, c.config); err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func serveGif(res http.ResponseWriter, req *http.Request) {
+func (c *CoffeeEngine) serveGif(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Pragma-directive", "no-cache")
 	res.Header().Set("Cache-directive", "no-cache")
 	res.Header().Set("Cache-control", "no-store")
@@ -49,7 +75,7 @@ func serveGif(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Expires", "0")
 	var files []string
 
-	root := "./content/img"
+	root := c.config.GifDir
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if strings.HasSuffix(path, ".gif") {
 			files = append(files, path)
